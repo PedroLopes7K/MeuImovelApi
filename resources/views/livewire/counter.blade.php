89d3fb2814d83da961936b555288a00434835b9c
PedroLopes7K <pedrohenri3601@gmail.com>
2022-08-14 21:04:38 -0300

    livewire component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h3>Hello LiveWire!</h3>
+    <div style="text-align: center">
+        <button wire:click="increment">+</button>
+        <button wire:click="decrement">-</button>
+        <h1>{{ $counter }}</h1>
+    </div>
+</div>
